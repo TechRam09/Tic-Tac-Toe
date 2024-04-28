da8486476e654e31ae7b16c0562dba06626c719e
Shreeram N S <nsshreeram@gmail.com>
2024-04-28 22:52:43 +0530

    Update game.js
    declared variable and got reference from html

---
+++ code/views/JS/game.js
@@ -0,0 +1,7 @@
+let boxes = document.querySelectorAll(".box");
+let resetBtn = document.getElementById("reset");
+let newGame = document.getElementById("newGame");
+let game = document.querySelector(".game");
+let winnerText = document.querySelector(".winnerText");
+let container = document.querySelector(".container");
+let turnX = true;
