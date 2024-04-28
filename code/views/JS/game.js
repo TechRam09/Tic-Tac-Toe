0d0879119df2dd89617dd16175c65ef7036c30b5
Shreeram N S <nsshreeram@gmail.com>
2024-04-28 22:54:06 +0530

    Update game.js
    declared winning pattern

--- code/views/JS/game.js
+++ code/views/JS/game.js
@@ -5,3 +5,13 @@ let game = document.querySelector(".game");
 let winnerText = document.querySelector(".winnerText");
 let container = document.querySelector(".container");
 let turnX = true;
+let winningPattern = [
+  [0, 1, 2],
+  [0, 3, 6],
+  [0, 4, 8],
+  [1, 4, 7],
+  [2, 5, 8],
+  [2, 4, 6],
+  [3, 4, 5],
+  [6, 7, 8],
+];
